a0fe83958bc5e667d8e954456dcea1d334caa370
Solomon <solomonmeresa8@gmail.com>
2020-10-14 17:32:40 -0700

    adds login  authentication

--- models/User.js
+++ models/User.js
@@ -3,7 +3,13 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+
+    // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
     {
@@ -47,7 +53,7 @@ User.init(
             }
           },
       sequelize,
-      timestamps: false,
+      timestamps: false,    
       freezeTableName: true,
       underscored: true,
       modelName: 'user'
