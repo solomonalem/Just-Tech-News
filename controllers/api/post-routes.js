3576f85c42d4f23f5649197bf82d598639d7ac37
Solomon <solomonmeresa8@gmail.com>
2020-10-21 17:57:00 -0700

    adds single post page

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -78,7 +78,7 @@ router.get('/:id', (req, res) => {
         res.status(500).json(err);
       });
   });
-  router.post('/', (req, res) => {
+router.post('/', (req, res) => {
     // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
     Post.create({
       title: req.body.title,
@@ -94,13 +94,16 @@ router.get('/:id', (req, res) => {
 
   // PUT /api/posts/upvote
   router.put('/upvote', (req, res) => {
-    // custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-      .then(updatedPostData => res.json(updatedPostData))
-      .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-      });
+    // make sure the session exists first
+    if (req.session) {
+      // pass session id along with all destructured properties on req.body
+      Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedVoteData => res.json(updatedVoteData))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    }
   });
 
   // UPDATE ----
